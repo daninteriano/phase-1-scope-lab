0cc2d03a0c571ce41647888aa540c2fcc1a3c475
Daniel Interiano <dannyboyint@gmail.com>
2021-07-29 15:16:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(newName){
+    var newName = customerName.toUpperCase();
+    let customerName = newName;
+    return customerName;
+}
