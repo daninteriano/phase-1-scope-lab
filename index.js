ad2e42d40b7ac49139701d3fceea53a289fe72f2
Daniel Interiano <dannyboyint@gmail.com>
2021-07-29 15:43:17 -0400

    commit tree 26c0919f1ece80fa1aae6972bce7b531f0d1202d

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ var customerName = 'bob';
 
 function upperCaseCustomerName(newName){
     var newName = customerName.toUpperCase();
-    let customerName = newName;
+    customerName = newName;
     return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'stan';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'jim';
+    return leastFavoriteCustomer;
 }
